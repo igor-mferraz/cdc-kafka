0b1ee38cd9f0b3df7098ad1ebc065304d97b8c4e
Igor <igormanoelferraz2022@gmail.com>
2025-03-30 00:11:20 -0300

    reset no banco e gerado migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
+import AppDataSource from 'typeorm.config';
+
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost', 
-      port: 3306,
-      username: 'my-sql',
-      password: 'postgres',
-      database: 'db-mysql',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, 
-    }),
+    TypeOrmModule.forRoot(AppDataSource.options),
     UsersModule,
   ],
 
--- typeorm.config.ts
+++ typeorm.config.ts
@@ -1,18 +1,14 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { DataSource } from 'typeorm';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
+const AppDataSource = new DataSource({
   type: 'mysql',
-  host: process.env.DATABASE_HOST || 'mysql',
-  port: parseInt(process.env.DATABASE_PORT) || 3306,
-  username: process.env.MYSQL_USER || 'my-sql',
-  password: process.env.MYSQL_PASSWORD || '12345678',
-  database: process.env.MYSQL_DATABASE || 'db-mysql',
-  entities: ['src/**/*.entity{.ts,.js}'],
+  host: 'localhost',
+  port: 3306,
+  username: 'my-sql',
+  password: 'postgres',
+  database: 'db-mysql',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: ['src/migrations/**/*{.ts,.js}'],
-}; 
+});
 
-console.log('MYSQL_USER:', process.env.MYSQL_USER || 'my-sql');
-console.log('MYSQL_PASSWORD:', process.env.MYSQL_PASSWORD);
-console.log('MYSQL_HOST:', process.env.DATABASE_HOST || 'db-mysql');
-console.log('MYSQL_PORT:', process.env.DATABASE_PORT || 3306);
-console.log('MYSQL_DATABASE:', process.env.MYSQL_DATABASE || 'db-mysql');
+export default AppDataSource;
